66123177efe2771ff651037f04127ebfca24fcad
Ian Esteves do Nascimento <ianesteves5@gmail.com>
2016-05-22 10:01:38 -0300

    Refactoring

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -10,6 +10,8 @@ class Board:
          self.groups.append(CellGroup([self.cells[j][i] for j in range(9)]))
       for i, j in ([x, y] for x in range(0, 9, 3) for y in range(0, 9, 3)):
          self.groups.append(CellGroup([self.cells[x][y] for x in range(i, i+3) for y in range(j, j+3)]))
+   def __iter__(self):
+      return (self.cells[x][y] for x in range(9) for y in range(9))
    def reset(self):
       for c in (y for x in self.cells for y in x):
          c.reset()
@@ -21,8 +23,12 @@ class Board:
    def solve(self):
       if not self.check():
          return False
-      for c in (y for x in self.cells for y in x):
-         c.cleanGroups()
+      try:
+         for c in (y for x in self.cells for y in x):
+            c.cleanGroups()
+      except BaseException as e:
+         print(str(e))
+         return False
       if not self.check():
          return False
       return True
@@ -45,6 +51,8 @@ class Cell:
       self.foundCallback()
       self.cleanGroups()
    def forceSetValue(self, v):
+      if v<1 or v>9:
+         return
       self.values = [v]
       self.changeCallback()
       self.foundCallback()
@@ -99,65 +107,71 @@ class CellGroup:
 class GameWindow(tkinter.Tk):
    def __init__(self, parent=None, board=None):
       tkinter.Tk.__init__(self, parent)
+      self.title("Sudoku Solver")
       self.parent = parent
-      if board==None:
-         self.init(Board())
-      else:
-         self.init(board)
-      
-   def init(self, board):
-      self.board = board
-      self.selectedCell = None
-      self.selectedButton = None
       self.boardGroup = tkinter.Frame(self, background="gray")
       self.buttonGroups = [[tkinter.Frame(self.boardGroup) for x in range(3)] for y in range(3)]
       self.buttons = [[tkinter.Button(self, width=5, height=3, borderwidth=2) for x in range(9)] for y in range(9)]
       self.textField = tkinter.Label(self)
-      
-      self.bind("<Key>", lambda event: self.onKeyPress(event.char))
-      self.bind("<Return>", lambda event: self.onReturnPress())
-      self.bind("<space>", lambda event: self.onSpacePress())
-      self.bind("<Escape>", lambda event: self.destroy())
-      
       self.grid()
       self.boardGroup.grid(row=0, column=0)
       for bg, i, j in ([self.buttonGroups[x][y], x, y] for x in range(3) for y in range(3)):
          bg.grid(column=i, row=j, padx=1, pady=1)
-      for b, c, i, j in ([self.buttons[x][y], self.board.cells[x][y], x, y] for x in range(9) for y in range(9)):
+      for b, i, j in ([self.buttons[x][y], x, y] for x in range(9) for y in range(9)):
          b.grid(in_=self.buttonGroups[i//3][j//3],column=i%3, row=j%3)
          b.bind("<Button-1>", lambda event, i=i, j=j: self.onCellClick(i, j))
          b.bind("<Button-3>", lambda event, i=i, j=j: self.onCellRightClick(i, j))
-         c.changeCallback = lambda b=b, c=c: b.config(text=c.toString())
       self.textField.grid(row=1, column=0)
       self.resizable(False, False)
+      self.bind("<Key>", lambda event: self.onKeyPress(event.char))
+      self.bind("<Return>", lambda event: self.onReturnPress())
+      self.bind("<space>", lambda event: self.animationOff())
+      self.bind("<Escape>", lambda event: self.destroy())
+      if board==None:
+         self.load(Board())
+      else:
+         self.load(board)
+   def load(self, board):
+      self.board = board
+      self.selectedCell = None
+      self.selectedButton = None
+      self.textField.config(text="")
+      self.animationOff()
+      for b, c in ([self.buttons[x][y], self.board.cells[x][y]] for x in range(9) for y in range(9)):
+         c.changeCallback = lambda b=b, c=c: b.config(text=c.toString())
+         c.changeCallback()
    def onCellClick(self, row, column):
       self.selectedCell = self.board.cells[row][column]
       self.selectedButton = self.buttons[row][column]
-      self.textField.config(text=str(self.selectedCell.values))
+      self.setText(self.selectedCell.values)
    def onCellRightClick(self, row, column):
       self.selectedCell = self.board.cells[row][column]
       self.selectedButton = self.buttons[row][column]
       self.selectedCell.reset()
-      self.textField.config(text=str(self.selectedCell.values))
+      self.setText(self.selectedCell.values)
    def onKeyPress(self, key):
       if not key.isdigit() or key=="0" or self.selectedCell==None:
          return
       self.selectedCell.forceSetValue(int(key))
-      self.textField.config(text=str(self.selectedCell.values))
+      self.setText(self.selectedCell.values)
    def onReturnPress(self):
-      self.textField.config(text="Working...")
-      for c in (self.board.cells[x][y] for x in range(9) for y in range(9)):
-         c.foundCallback = lambda: self.onFound()
+      self.setText("Working...")
+      self.animationOn()
       if self.board.solve():
-         self.textField.config(text="Done!")
+         self.setText("Done!")
       else:
-         self.textField.config(text="Error")
-   def onSpacePress(self):
-      for c in (self.board.cells[x][y] for x in range(9) for y in range(9)):
-         c.foundCallback = lambda: None
+         self.setText=("Error")
    def onFound(self):
       self.update()
       self.after(150)
+   def animationOn(self):
+      for c in self.board:
+         c.foundCallback = lambda: self.onFound()
+   def animationOff(self):
+      for c in self.board:
+         c.foundCallback = lambda: None
+   def setText(self, text):
+      self.textField.config(text=str(text))
 
 if __name__ == "__main__":
    GameWindow().mainloop()
